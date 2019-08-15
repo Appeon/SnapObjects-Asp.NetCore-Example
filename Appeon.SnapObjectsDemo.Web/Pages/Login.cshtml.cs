df568c13b0da6b855ab5b729b2a0605cdae1823e
github@appeon.com <github@appeon.com>
2019-08-14 19:46:39 -0700

    update

---
+++ Appeon.SnapObjectsDemo.Web/Pages/Login.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Appeon.MvcModelMapperDemo.Models;
+using Appeon.SnapObjectsDemo.Service.Models;
+using Appeon.SnapObjectsDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Appeon.MvcModelMapperDemo.Pages
+{
+    public class LoginModel : BasePageModel
+    {
+        private readonly ILoginService loginService;
+        public LoginModel(ILoginService _loginService)
+        {
+            this.loginService = _loginService;
+        }
+
+        [BindProperty]
+        public Login Login { get; set; }
+
+        public void OnGet()
+        {
+            Login = new Login();
+            Login.Password = "K7dMpTY=";
+            Login.Firstname = "Michael.Raheem";
+        }
+
+        public IActionResult OnPost()
+        {
+            try
+            {
+                bool isExist = loginService.UserIsExist(Login.Firstname);
+                if (isExist)
+                {
+                    bool isLogin = loginService.Login(Login.Firstname, Login.Password);
+                    if (!isLogin)
+                    {
+                        return GenJsonResult(-1, "Login fail,The user name or password is not valid.", 0);
+                    }
+                    else
+                    {
+                        //save session
+                        byte[] b_login = System.Text.Encoding.Default.GetBytes(Login.Firstname);
+                        HttpContext.Session.Set("loginName", b_login);
+                    }
+                }
+                else
+                {
+                    return GenJsonResult(-1, "Login fail,The user is not exist.", 0);
+                }
+            }
+            catch (Exception e)
+            {
+                return GenJsonResult(-1, e.Message, 0);
+            }
+
+            return GenJsonResult(1, "", 0);
+        }
+    }
+}
